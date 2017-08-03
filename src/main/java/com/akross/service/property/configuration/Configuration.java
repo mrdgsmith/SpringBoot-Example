e7f11087c283c27f967477ce72ab3eac184d3cda
Dean Smith <dc4@outlook.com>
2017-08-03 17:41:57 +0100

    https://trello.com/c/NVdxUd7F - Not needed yet

--- src/main/java/com/akross/service/property/configuration/Configuration.java
+++ src/main/java/com/akross/service/property/configuration/Configuration.java
@@ -1,15 +1,12 @@
 package com.akross.service.property.configuration;
 
-import com.akross.gateway.PropertyClient;
-import com.akross.service.PropertyService;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootConfiguration
 public class Configuration {
 
-    @Bean
-    public PropertyService propertyConverter(final PropertyClient propertyClient) {
-        return new com.akross.service.property.PropertyService(propertyClient);
-    }
+//    @Bean
+//    public PropertyService propertyConverter(final PropertyClient propertyClient) {
+//        return new com.akross.service.property.PropertyService(propertyClient);
+//    }
 }
