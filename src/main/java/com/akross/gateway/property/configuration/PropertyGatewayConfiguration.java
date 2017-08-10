88c89b96efcb1936619b77442fe796c5e08518d0
Dean Smith <dc4@outlook.com>
2017-08-10 17:34:25 +0100

    Refactor - Removing comments

--- src/main/java/com/akross/gateway/property/configuration/PropertyGatewayConfiguration.java
+++ src/main/java/com/akross/gateway/property/configuration/PropertyGatewayConfiguration.java
@@ -34,17 +34,4 @@ public class PropertyGatewayConfiguration {
             , final PropertyConverter propertyConverter) {
         return new com.akross.gateway.property.PropertyClient(httpPropertyClient, propertyConverter);
     }
-
-//    private static MappingJackson2XmlHttpMessageConverter mappingJacksonHttpMessageConverter() {
-//        final JacksonXmlModule jacksonXmlModule = new JacksonXmlModule();
-//        jacksonXmlModule.addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE)
-//                .addDeserializer(LocalTime.class, LocalTimeDeserializer.INSTANCE)
-//                .addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(ofPattern("yyyy-MM-dd HH:mm:ss")));
-//        final XmlMapper xmlMapper = (XmlMapper) new XmlMapper(jacksonXmlModule)
-//                .disable(FAIL_ON_UNKNOWN_PROPERTIES);
-//        MappingJackson2XmlHttpMessageConverter mappingJackson2XmlHttpMessageConverter
-//                = new MappingJackson2XmlHttpMessageConverter();
-//        mappingJackson2XmlHttpMessageConverter.setObjectMapper(xmlMapper);
-//        return mappingJackson2XmlHttpMessageConverter;
-//    }
 }
