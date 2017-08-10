2de4747968e172b082bad99aa78f421e989123a0
Dean Smith <dc4@outlook.com>
2017-08-11 00:15:07 +0100

    https://trello.com/c/cxT4tsdj - Update database ever hour

--- src/main/java/com/akross/service/property/schedule/SchedulePropertyLoader.java
+++ src/main/java/com/akross/service/property/schedule/SchedulePropertyLoader.java
@@ -3,20 +3,25 @@ package com.akross.service.property.schedule;
 import com.akross.gateway.PropertyClient;
 import com.akross.repository.PropertyRepository;
 import com.akross.repository.property.entity.Property;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Objects;
 
+@Component
+@EnableScheduling
 public class SchedulePropertyLoader {
 
-    private final PropertyClient propertyClient;
-    private final PropertyRepository propertyRepository;
+    @Autowired
+    private PropertyClient propertyClient;
 
-    public SchedulePropertyLoader(final PropertyClient propertyClient, final PropertyRepository propertyRepository) {
-        this.propertyClient = propertyClient;
-        this.propertyRepository = propertyRepository;
-    }
+    @Autowired
+    private PropertyRepository propertyRepository;
 
+    @Scheduled(fixedRate = 6000 * 60)
     public void getProperties() {
         final List<? extends Property> properties = propertyClient.getProperties();
         if (Objects.nonNull(properties) && !properties.isEmpty()) {
